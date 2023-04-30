027e884bd232e5bffdda3c86bc3498c41c8eef11
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-04-30 17:50:02 +0200

    Convert userEta input in dropdown menu with option

--- js/main.js
+++ js/main.js
@@ -38,21 +38,35 @@ function miaFunzione() {
     // Calcolo prezzo finale
     calcPrice = howKm * euroKm;
 
-    if (userEta < 18) { // sconto minorenni
+    if (userEta === "minorenne") { // sconto minorenni
         finalPrice = calcPrice - (calcPrice * 0.20);
         console.log("prezzo scontato 20% (poichè minorenne) =", finalPrice, "€");
+        document.getElementById("tipologiaBiglietto").innerHTML = "Sconto minorenni";
 
-    } else if (userEta > 65) { // sconto over 65
+    } else if (userEta === "old") { // sconto over 65
         finalPrice = calcPrice - (calcPrice * 0.40);
         console.log("prezzo scontato 40% (poichè over 65) =", finalPrice, "€");
+        document.getElementById("tipologiaBiglietto").innerHTML = "Sconto over 65";
 
     } else { // sconto (nullo) tra 18-65 anni
         finalPrice = calcPrice;
         console.log("prezzo senza sconto (poichè tra 18-65 anni) =", finalPrice, "€");
+        document.getElementById("tipologiaBiglietto").innerHTML = "Tariffa standard";
     }
 
     // Genero messaggio a schermo
-    document.getElementById("mio_id").innerHTML = finalPrice.toFixed(2) + "€";
+
+    const prezzoBarrato = document.querySelector("h4#prezzoBarrato");
+
+    if (userEta === "minorenne" || userEta === "old") { // sconto minorenni
+        document.getElementById("prezzo").innerHTML = finalPrice.toFixed(2) + "€";
+        document.getElementById("prezzoBarrato").innerHTML = calcPrice.toFixed(2) + "€";
+        prezzoBarrato.className = "add";
+    } else {
+        document.getElementById("prezzo").innerHTML = finalPrice.toFixed(2) + "€";
+        prezzoBarrato.className = "remove";
+    }
+
     document.getElementById("mostroNomePasseggero").innerHTML = userName;
 
 }
